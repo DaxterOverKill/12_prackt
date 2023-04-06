11df87cab764b581d8f875937a3b25c66916711c
Mae-lv <focus9ka1@mail.ru>
2023-04-06 16:48:58 +0300

    Fixed bugs

--- app/main.py
+++ app/main.py
@@ -14,47 +14,6 @@ def add_document(content: CreateDocumentModule):
 
 app = FastAPI()
 
-#
-###########
-#Jaeger
-
-from opentelemetry import trace
-from opentelemetry.exporter.jaeger.thrift import JaegerExporter
-from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
-from opentelemetry.sdk.resources import SERVICE_NAME, Resource
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor
-
-resource = Resource(attributes={
-    SERVICE_NAME: 'docs-service'
-})
-
-jaeger_exporter = JaegerExporter(
-    agent_host_name = "jaeger",
-    agent_port = 6831,
-)
-
-provider = TracerProvider(resource=resource)
-processor = BatchSpanProcessor(jaeger_exporter)
-provider.add_span_processor(processor)
-trace.set_tracer_provider(provider)
-
-FastAPIInstrumentor.instrument_app(app)
-
-#
-###############
-
-##############
-#  Prometheus
-
-from prometheus_fastapi_instrumentator import Instrumentator
-
-@app.on_event("startup")
-async def startup():
-    Instrumentator().instrument(app).expose(app)
-
-#
-################
 @app.get("/v1/docs")
 async def get_docs():
     return documents
